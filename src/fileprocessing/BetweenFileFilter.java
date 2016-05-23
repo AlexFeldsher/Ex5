8ad6545e6373c1f95f62675c27e2537d05b089ab
alex <test@test.com>
2016-05-23 13:28:43 +0300

    Changed field to final Added between filter

---
+++ src/fileprocessing/BetweenFileFilter.java
@@ -0,0 +1,33 @@
+package fileprocessing;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * Filters files two given file size value
+ */
+public class BetweenFileFilter implements FileFilter {
+
+    /* Hold the lower and upper limit values */
+    private final double LOWER_LIMIT;
+    private final double UPPER_LIMIT;
+
+    /**
+     * Constructor for between file filter
+     *
+     * @param x lower limit file size
+     * @param y upper limit file size
+     */
+    public BetweenFileFilter(double x, double y) {
+        LOWER_LIMIT = x;
+        UPPER_LIMIT = y;
+    }
+
+    public boolean accept(File file) {
+        double fileSizeKB = file.length() / 1024;
+        if (fileSizeKB >= LOWER_LIMIT && fileSizeKB <= UPPER_LIMIT) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/fileprocessing/GreaterThanFileFilter.java
+++ src/fileprocessing/GreaterThanFileFilter.java
@@ -4,24 +4,23 @@ import java.io.File;
 import java.io.FileFilter;
 
 /**
- * Filters files greater than a given value
+ * Filters files greater than a given file size value
  */
 public class GreaterThanFileFilter implements FileFilter {
     /* holds the value to compare to */
-    private double greaterThanValue;
+    private final double GREATER_THAN_VALUE;
 
     /**
-     * Constructor for
-     *
+     * Constructor for greater than file filter
      * @param value the value that should be filtered against
      */
     public GreaterThanFileFilter(double value) {
-        greaterThanValue = value;
+        GREATER_THAN_VALUE = value;
     }
 
-    public boolean accept(File pathname) {
-        double fileSizeKB = pathname.length() / 1024;
-        if (fileSizeKB > greaterThanValue) {
+    public boolean accept(File file) {
+        double fileSizeKB = file.length() / 1024;
+        if (fileSizeKB > GREATER_THAN_VALUE) {
             return true;
         }
         return false;
