8924195839163ca440b9b82f886a2a85158e0b12
alex <test@test.com>
2016-05-24 00:40:34 +0300

    new FileSizeComparator

---
+++ src/fileprocessing/comperators/FileSizeComparator.java
@@ -0,0 +1,18 @@
+package fileprocessing.comperators;
+
+import java.io.File;
+import java.util.Comparator;
+
+public class FileSizeComparator implements Comparator<File> {
+    public int compare(File file1, File file2) {
+        long fileSize1 = file1.length();
+        long fileSize2 = file2.length();
+        if (fileSize1 < fileSize2) {
+            return -1;
+        }
+        if (fileSize1 > fileSize2) {
+            return 1;
+        }
+        return 0;
+    }
+}
