15b9f916ff5fbd7078ba397039a8abfbc757fcae
alex <test@test.com>
2016-05-23 13:54:51 +0300

    added suffix file filter

---
+++ src/fileprocessing/SuffixFileFilter.java
@@ -0,0 +1,28 @@
+package fileprocessing;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * Filters files that their name end with a given suffix
+ */
+public class SuffixFileFilter implements FileFilter {
+    /* holds the suffix to filter with */
+    private final String SUFFIX;
+
+    /**
+     * Suffix file filter constructor
+     *
+     * @param suffix the suffix to filter against
+     */
+    public SuffixFileFilter(String suffix) {
+        SUFFIX = suffix;
+    }
+
+    public boolean accept(File file) {
+        if (file.getName().endsWith(SUFFIX)) {
+            return true;
+        }
+        return false;
+    }
+}
